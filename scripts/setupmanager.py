80e62d2bb104180a9529c717697cef27e6ca6c24
Jay <jayraopatel@gmail.com>
2025-07-07 01:38:45 -0700

    Add setupmanager.py

---
+++ scripts/setupmanager.py
@@ -0,0 +1,30 @@
+"""
+SetupManager class for handling project dependencies and ffmpeg installation.
+"""
+
+class SetupManager:
+
+    def __init__(self):
+        pass
+
+    def print_status(self, message):
+        pass
+
+    def check_pip_dependencies(self):
+        pass
+
+    def is_package_installed(self, package_name):
+        pass
+
+    def check_ffmpeg_installation(self):
+        pass
+
+    def prompt_local_ffmpeg_install(self):
+        pass
+
+    def install_local_ffmpeg(self):
+        pass
+
+    def run_setup(self):
+        pass
+
